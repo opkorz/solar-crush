39b04d8319425627c04a4eaf58f781504365758b
Matt <matthew.leane@gmail.com>
2016-04-09 09:56:55 +1000

    Added base html and javascript file

---
+++ solar_crush/static-raw/game.js
@@ -0,0 +1,15 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
+
+var view = 0; //0 for main menu, 1 for overview, 2 for inside
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update(){
+
+}
